259b69fa4ca149d562a6b049d9b266f050a051eb
DVD-R-2 <92395016+TrojanDotEXE@users.noreply.github.com>
2023-02-09 19:36:27 +0200

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Main.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -7,7 +8,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 
 @TeleOp(group = "1", name = "Main")
-public class Main extends OpMode {
+public class Main extends LinearOpMode {
     Robot robot = new Robot(this);
     Gamepad currentGamepad1;
     Gamepad previousGamepad1;
@@ -15,31 +16,38 @@ public class Main extends OpMode {
     Gamepad previousGamepad2;
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
         robot.init();
         currentGamepad1 = new Gamepad();
         previousGamepad1 = new Gamepad();
         currentGamepad2 = new Gamepad();
         previousGamepad2 = new Gamepad();
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()){
+            previousGamepad1.copy(currentGamepad1);
+            currentGamepad1.copy(gamepad1);
+            previousGamepad2.copy(currentGamepad2);
+            currentGamepad2.copy(gamepad2);
+
+            robot.intake.keyBind(currentGamepad2, previousGamepad2);
+        }
     }
 
-    @Override
-    public void start(){
-        telemetry.clear();
-        telemetry.update();
-    }
 
-    @Override
-    public void loop() {
-        previousGamepad1.copy(currentGamepad1);
-        currentGamepad1.copy(gamepad1);
-        previousGamepad2.copy(currentGamepad2);
-        currentGamepad2.copy(gamepad2);
 
-//        robot.drivetrain.drive(currentGamepad1, previousGamepad1);
-        robot.intake.keyBind(currentGamepad2, previousGamepad2);
-//        robot.scoring.keyBind(currentGamepad2, previousGamepad2);
-//        robot.scoring.getPos();
-        telemetry.update();
-    }
+//    @Override
+//    public void loop() {
+//        previousGamepad1.copy(currentGamepad1);
+//        currentGamepad1.copy(gamepad1);
+//        previousGamepad2.copy(currentGamepad2);
+//        currentGamepad2.copy(gamepad2);
+//
+////        robot.drivetrain.drive(currentGamepad1, previousGamepad1);
+//        robot.intake.keyBind(currentGamepad2, previousGamepad2);
+////        robot.scoring.keyBind(currentGamepad2, previousGamepad2);
+////        robot.scoring.getPos();
+//        telemetry.update();
+//    }
 }
