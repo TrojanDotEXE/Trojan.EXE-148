8e610a31cac01f7c18cd62b7e1c3dd7035e001f5
SnowyLolz <106928133+SnowyLolz@users.noreply.github.com>
2023-02-09 19:20:14 +0200

    da

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Main3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Main3.java
@@ -46,7 +46,10 @@ public class Main3 extends LinearOpMode {
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
-
+        currentGamepad1 = new Gamepad();
+        previousGamepad1 = new Gamepad();
+        currentGamepad2 = new Gamepad();
+        previousGamepad2 = new Gamepad();
 
         rightClaw = hardwareMap.get(Servo.class, "clawR");
         leftClaw = hardwareMap.get(Servo.class, "clawL");
@@ -67,10 +70,9 @@ public class Main3 extends LinearOpMode {
         slider2.setDirection(Servo.Direction.FORWARD);
         slider1.setPosition(RETRACTED_S);
         slider2.setPosition(RETRACTED_D);
-        currentGamepad1 = new Gamepad();
-        previousGamepad1 = new Gamepad();
-        currentGamepad2 = new Gamepad();
-        previousGamepad2 = new Gamepad();
+
+        waitForStart();
+
         while (opModeIsActive() && !isStopRequested()){
             previousGamepad1.copy(currentGamepad1);
             currentGamepad1.copy(gamepad1);
