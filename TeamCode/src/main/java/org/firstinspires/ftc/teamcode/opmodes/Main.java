eaf396d2c5e7127cf9063c2a519b1e3e48557b4b
DVD-R-2 <92395016+TrojanDotEXE@users.noreply.github.com>
2023-02-09 19:39:18 +0200

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Main.java
@@ -10,24 +10,24 @@ import org.firstinspires.ftc.teamcode.robot.Robot;
 @TeleOp(group = "1", name = "Main")
 public class Main extends LinearOpMode {
     Robot robot = new Robot(this);
-    Gamepad currentGamepad1;
-    Gamepad previousGamepad1;
+//    Gamepad currentGamepad1;
+//    Gamepad previousGamepad1;
     Gamepad currentGamepad2;
     Gamepad previousGamepad2;
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init();
-        currentGamepad1 = new Gamepad();
-        previousGamepad1 = new Gamepad();
+//        currentGamepad1 = new Gamepad();
+//        previousGamepad1 = new Gamepad();
         currentGamepad2 = new Gamepad();
         previousGamepad2 = new Gamepad();
 
         waitForStart();
 
         while (opModeIsActive() && !isStopRequested()){
-            previousGamepad1.copy(currentGamepad1);
-            currentGamepad1.copy(gamepad1);
+//            previousGamepad1.copy(currentGamepad1);
+//            currentGamepad1.copy(gamepad1);
             previousGamepad2.copy(currentGamepad2);
             currentGamepad2.copy(gamepad2);
 
